2eb149827a7ea2d9ee341baabbf8bedf1a7e4254
Rohit Shinde <rohitshinde0550@gmail.com>
2025-02-14 15:44:56 +0530

    Updated the page with new styles and data for multiple experiences

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,163 @@
 <template>
-  <div class="font-sans bg-gray-100">
-    <!-- Header Section -->
-    <header class="bg-blue-600 text-white py-6 text-center">
-      <h1 class="text-4xl font-bold">Rohit Shinde</h1>
-      <p class="text-xl">Senior Test Automation Engineer</p>
-    </header>
+  <div class="min-h-screen bg-gray-100 flex justify-center py-10 px-5">
+    <div class="max-w-4xl w-full bg-white shadow-lg rounded-lg p-8">
+      <header class="text-left border-b pb-6">
+        <h1 class="text-4xl font-bold text-gray-900">ROHIT SHINDE</h1>
+        <p class="text-lg text-gray-600 mt-2">SENIOR TEST AUTOMATION ENGINEER</p>
+      </header>
 
-    <!-- About Me Section -->
-    <section class="container mx-auto px-4 py-8">
-      <h2 class="text-2xl font-bold text-blue-600 mb-4">About Me</h2>
-      <p class="text-gray-700">
-        As a results-driven Automation and Manual Test Engineer, I bring over 12 years of expertise in enhancing software quality through comprehensive testing strategies. My specialization spans web automation, API testing, and performance testing, leveraging tools such as Selenium WebDriver, Cypress, Java, JavaScript, TestNG, and Cucumber.
-      </p>
-    </section>
+      <div class="grid grid-cols-3 gap-6 mt-6">
+        <!-- Left Column -->
+        <div class="col-span-1 space-y-6">
+          <section>
+            <h2 class="text-lg font-semibold text-gray-700">CONTACTS</h2>
+            <div class="mt-2 space-y-2">
+              <div class="flex items-center text-gray-600">
+                <i class="fas fa-map-marker-alt text-lg mr-2"></i>
+                <span>Pune, Maharashtra, India</span>
+              </div>
+              <div class="flex items-center text-gray-600">
+                <i class="fas fa-envelope text-lg mr-2"></i>
+                <span>rsrohitshindeg1@gmail.com</span>
+              </div>
+              <div class="flex items-center text-gray-600">
+                <i class="fas fa-phone-alt text-lg mr-2"></i>
+                <span>+91 90 28 533 209</span>
+              </div>
+            </div>
+          </section>
 
-    <!-- Work Experience Section -->
-    <section class="container mx-auto px-4 py-8">
-      <h2 class="text-2xl font-bold text-blue-600 mb-4">Work Experience</h2>
-      <div class="bg-white shadow-md rounded-lg p-6 mb-6">
-        <h3 class="text-xl font-semibold">Senior Test Automation Engineer</h3>
-        <p class="text-gray-600"><strong>Globant, Pune</strong> | DEC 2020 - Present</p>
-        <ul class="list-disc list-inside mt-4">
-          <li>Collaborated with the Partner Travel team to ensure the quality of applications designed for customers of financial institutions like FIS and Mastercard.</li>
-          <li>Utilized WebdriverIO with JavaScript for automation testing and employed JIRA with the Xray plugin for manual test case development and execution.</li>
-        </ul>
-      </div>
-      <!-- Add more job entries here -->
-    </section>
+          <section>
+            <h2 class="text-lg font-semibold text-gray-700">ABOUT ME</h2>
+            <p class="text-gray-600 mt-2 text-sm">
+              As a results-driven Automation and Manual Test Engineer, I bring over 12+ years of expertise in enhancing
+              software quality through comprehensive testing strategies. My specialization spans UI automation testing,
+              API automation testing, and Performance testing, leveraging tools such as Selenium WebDriver, Cypress, k6,
+              TestNG, Cucumber with programming languages Java and JavaScript.
+            </p>
+          </section>
 
-    <!-- Skills Section -->
-    <section class="container mx-auto px-4 py-8">
-      <h2 class="text-2xl font-bold text-blue-600 mb-4">Skills</h2>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <div class="bg-white shadow-md rounded-lg p-6">
-          <h3 class="text-lg font-semibold">Web Automation Testing</h3>
+          <section>
+            <h2 class="text-lg font-semibold text-gray-700">SKILLS</h2>
+            <div class="mt-2">
+              <p class="text-gray-600 text-sm">Selenium WebDriver</p>
+              <p class="text-gray-600 text-sm">Cypress</p>
+              <p class="text-gray-600 text-sm">Java</p>
+              <p class="text-gray-600 text-sm">JavaScript</p>
+              <p class="text-gray-600 text-sm">API Testing</p>
+              <p class="text-gray-600 text-sm">Performance Testing</p>
+              <p class="text-gray-600 text-sm">TestNG</p>
+              <p class="text-gray-600 text-sm">Cucumber</p>
+            </div>
+          </section>
+
+          <section>
+            <h2 class="text-lg font-semibold text-gray-700">Links</h2>
+            <div class="mt-2 space-y-2">
+              <a href="https://linkedin.com/in/rohit-shinde-498a05a7" target="_blank"
+                class="flex items-center text-gray-600 hover:text-blue-600 transition duration-300">
+                <i class="fab fa-linkedin text-xl mr-2"></i>
+                <span class="underline">LinkedIn</span>
+              </a>
+              <a href="https://github.com/rsrohit" target="_blank"
+                class="flex items-center text-gray-600 hover:text-gray-900 transition duration-300">
+                <i class="fab fa-github text-xl mr-2"></i>
+                <span class="underline">GitHub</span>
+              </a>
+            </div>
+          </section>
         </div>
-        <div class="bg-white shadow-md rounded-lg p-6">
-          <h3 class="text-lg font-semibold">API Testing</h3>
-        </div>
-        <div class="bg-white shadow-md rounded-lg p-6">
-          <h3 class="text-lg font-semibold">Selenium WebDriver</h3>
-        </div>
-        <!-- Add more skills here -->
-      </div>
-    </section>
 
-    <!-- Education Section -->
-    <section class="container mx-auto px-4 py-8">
-      <h2 class="text-2xl font-bold text-blue-600 mb-4">Education</h2>
-      <div class="bg-white shadow-md rounded-lg p-6">
-        <p class="text-gray-700"><strong>Bachelor of Computer Application</strong> | Savitribai Phule Pune University</p>
-      </div>
-    </section>
+        <!-- Right Column -->
+        <div class="col-span-2">
+          <section>
+            <h2 class="text-lg font-semibold text-gray-700">WORK EXPERIENCE</h2>
 
-    <!-- Links Section -->
-    <section class="container mx-auto px-4 py-8">
-      <h2 class="text-2xl font-bold text-blue-600 mb-4">Links</h2>
-      <div class="flex space-x-4">
-        <a href="https://linkedin.com/in/rohit-shinde-498a05a7" target="_blank" class="text-blue-600 hover:underline">
-          <i class="fab fa-linkedin"></i> LinkedIn
-        </a>
-        <a href="https://github.com/rsrohit" target="_blank" class="text-blue-600 hover:underline">
-          <i class="fab fa-github"></i> GitHub
-        </a>
+            <div v-if="isLoading" class="flex justify-center mt-4">
+              <span class="text-gray-500 text-sm">Loading experience...</span>
+            </div>
+
+            <div v-else class="mt-4 space-y-4">
+              <div v-for="company in workExperience" :key="company.id">
+                <h3 class="text-md font-semibold text-gray-800">{{ company.name }}</h3>
+                <p class="text-sm text-gray-600 italic">{{ company.position }} ({{ company.period }})</p>
+                <div v-for="project in company.projects" :key="project.project" @click="openModal(project)"
+                  class="cursor-pointer p-4 border rounded-lg shadow-md hover:shadow-lg hover:bg-gray-50 transition mt-2">
+                  <p class="text-sm font-semibold">{{ project.project }}</p>
+                  <p class="text-xs text-gray-500">{{ project.location }}</p>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
       </div>
-    </section>
+    </div>
+
+    <!-- Modal for Work Experience Details -->
+    <div v-if="showModal" class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
+      <div class="bg-white w-3/4 max-w-lg rounded-lg p-6 shadow-lg max-h-[80vh] overflow-y-auto relative">
+        <button @click="closeModal" class="absolute top-4 right-4 text-gray-600" aria-label="Close Modal">✖</button>
+        <h2 class="text-xl font-bold text-gray-900 text-center">{{ selectedExperience.project }}</h2>
+        <p class="text-gray-600 text-sm text-center">{{ selectedExperience.location }}</p>
+        <div class="mt-4">
+          <ul class="list-disc text-gray-600 text-sm mt-2 ml-4">
+            <li v-for="point in selectedExperience.details" :key="point">{{ point }}</li>
+          </ul>
+          <h3 class="text-md font-semibold text-gray-800 mt-4">Skills Used</h3>
+          <p class="text-gray-600 text-sm">{{ selectedExperience.skills }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import experienceData from "@/experienceData.js";
+
 export default {
-  name: 'App',
-}
+  name: "ResumePage",
+  data() {
+    return {
+      showModal: false,
+      selectedExperience: {},
+      workExperience: [], // Empty initially
+      isLoading: true
+    };
+  },
+  mounted() {
+    // Simulating an API request with a delay
+    setTimeout(() => {
+      this.workExperience = experienceData;
+      this.isLoading = false;
+    }, 1000);
+  },
+  methods: {
+    openModal(company) {
+      this.selectedExperience = company;
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+    }
+  }
+};
 </script>
 
+
 <style>
-/* Add custom styles if needed */
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap');
+
+body {
+  font-family: 'Poppins', sans-serif;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
---
+++ src/experienceData.js
@@ -0,0 +1,96 @@
+// src/experienceData.js
+export default [
+    {
+        id: "globant",
+        name: "Globant India Pvt Ltd, Pune",
+        position: "Senior Test Automation Engineer",
+        period: "Dec 2020 - Present",
+        projects: [
+            {
+                project: "Partner Travel - JPMorgan Chase",
+                location: "Pune (Jul 2024 - Dec 2024)",
+                details: [
+                    "Ensured quality for Partner Travel applications supporting financial institutions like FIS and Mastercard.",
+                    "Used WebdriverIO with JavaScript for automation testing.",
+                    "Developed test plans and executed manual/automated testing.",
+                    "Maintained test documentation in JIRA and Confluence."
+                ],
+                skills: "WebdriverIO, JavaScript, JIRA, Xray, Confluence"
+            },
+            {
+                project: "MissionGraph - Deloitte US",
+                location: "Pune (Dec 2020 - Jun 2024)",
+                details: [
+                    "Led the QA efforts on the MissionGraph project, a robust big data ingestion and analytics platform developed for Deloitte.",
+                    "Spearheaded the implementation of automated software testing solutions, boosting test coverage by over 300% and elevating automated vs. manual coverage from 13% to 63%.",
+                    "Developed and maintained a suite of end-to-end (E2E) and API integration tests using the Cypress.io framework, ensuring comprehensive validation of platform functionality and interactions.",
+                    "Established seamless API integration with Cypress.io, GitHub, and Microsoft Teams, enabling immediate test result notifications and comments on Teams channels.",
+                    "Devised robust workflows utilizing GitHub Actions for SonarQube analysis and code coverage assessment across NodeJS, Python, and Java projects.",
+                    "Implemented hourly health check test runs to proactively monitor the health of staging, production environments, and the development environment data pipeline, enabling timely alerts and prompt corrective actions.",
+                    "Automated scripted load tests using Jenkins and K6, enhancing load testing efficiency and accuracy.",
+                    "Conducted comprehensive automated accessibility testing with the Cypress framework and the Cypress-axe plugin, ensuring compliance with accessibility standards and providing an inclusive user experience.",
+                    "Provided dynamic leadership by leading weekly QE meetings, crafting a comprehensive test automation strategy and roadmap, managing work items within JIRA, offering technical mentorship, and driving continuous quality engineering enhancements.",
+                    "Pioneered quality engineering processes that embraced a shift-left, agile approach, focusing on robust automated test coverage and introducing automated quality gates."
+                ],
+                skills: "Cypress.io, GitHub Actions, Jenkins, K6, SonarQube, Python, Java, JavaScript"
+            }
+        ]
+    },
+    {
+        id: "expleo",
+        name: "Expleo Group, Pune",
+        position: "QA Analyst",
+        period: "May 2017 - Feb 2020",
+        projects: [
+            {
+                project: "Siemens PLM Software",
+                location: "Pune",
+                details: [
+                    "Worked on manual and automation testing for Manufacturing Clients.",
+                    "Developed automation scripts using Selenium and Java.",
+                    "Implemented automated test cases using UFT for java based windows application.",
+                    "Worked closely with development teams to resolve defects."
+                ],
+                skills: "Selenium, Java, JMeter, Manual Testing, UFT, Polarion"
+            }
+        ]
+    },
+    {
+        id: "yardi",
+        name: "Yardi Software India Pvt Ltd, Pune",
+        position: "Specialist QC - II",
+        period: "Sep 2015 - May 2017",
+        projects: [
+            {
+                project: "Yardi - Property Management Solutions",
+                location: "Pune",
+                details: [
+                    "Tested property management applications for global clients.",
+                    "Developed test cases and executed automation using TestNG.",
+                    "Performed API testing using Postman and RestAssured.",
+                    "Collaborated with US-based clients for requirement analysis."
+                ],
+                skills: "TestNG, Postman, RestAssured, Selenium"
+            }
+        ]
+    },
+    {
+        id: "syntel",
+        name: "Syntel Ltd, Pune",
+        position: "Testing Engineer",
+        period: "Jul 2012 - Sep 2015",
+        projects: [
+            {
+                project: "FedEx Ground Team",
+                location: "Pune",
+                details: [
+                    "Worked on Logistics and Shipping processing systems of FedEx.",
+                    "Developed automated scripts using QTP/UFT.",
+                    "Executed regression testing and defect tracking in HP ALM.",
+                    "Ensured compliance with healthcare industry standards."
+                ],
+                skills: "QTP/UFT, HP ALM, Regression Testing"
+            }
+        ]
+    }
+];
